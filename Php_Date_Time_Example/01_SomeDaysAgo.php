949d36543554db3d8381c5e03349d6d52af30b5f
Magdalena Petrova <magik70@abv.bg>
2014-12-05 17:38:06 +0200

    Php_Date_Time_Example

---
+++ Php_Date_Time_Example/01_SomeDaysAgo.php
@@ -0,0 +1,40 @@
+<?php
+date_default_timezone_set('Europe/Sofia');
+$date1 = new DateTime();
+echo 'Now is - '. $date1->format('jS F, Y h:ia') . "<br/><br/>" . PHP_EOL;
+
+$ten_days_ago = new DateInterval( "P10D" );//10 days before
+$ten_days_ago->invert = 1; //Make it negative.
+$date1->add( $ten_days_ago );
+echo $date1->format('jS F, Y h:ia') . ' - is Now 10 days in the past.' . "<br/><br/>" . PHP_EOL;
+
+$date2 = new DateTime();
+$ten_days_after = new DateInterval( "P10D" );//10 days after
+$ten_days_after->invert = 0; //Make it positive.
+$date2->add( $ten_days_after );
+echo $date2->format('jS F, Y h:ia') . ' - is Now 10 days in the future.' . "<br/><br/>" . PHP_EOL;
+
+$date1->sub(new DateInterval('PT30M'));//thirty minutes from now
+echo $date1->format('jS F, Y h:ia') . ' - is Now 30 minutes in the past.' . "<br/><hr/>" . PHP_EOL;
+
+$now =  new DateTime();
+echo 'Today is - ' . $now->format('d-m-Y G:i:s') . "<br/><br/>";
+$future = new DateTime(' +10 year +0 months +10 days +10 minute +10 seconds');
+echo $future->format('d-m-Y G:i:s') . ' - that is (+10 year +10 days +10 minute +10 seconds) from Today.' . "<br/><br/>" . PHP_EOL;
+
+$past = new DateTime(' -10 year -0 months -10 days -10 minute -10 seconds');
+echo $past->format('d-m-Y G:i:s') . ' - that is (-10 year -10 days -10 minute -10 seconds) from Today.' . "<br/><hr/>" . PHP_EOL;
+
+$date3 = new DateTime();
+echo 'Today is - ' . $date3->format('d-m-Y G:i:s') . "<br/><br/>" . PHP_EOL;
+$interval = new DateInterval('P2Y3M4DT6H8M');
+$interval->invert = 0;
+$date3->add($interval);
+echo $interval->format('(%y +years, %m +mounts, %d +days, %h +hours, %i +minutes) ').'from today is ' . $date3->format('d-m-Y G:i:s') . "<br/><br/>" . PHP_EOL ;
+
+$date4 = new DateTime();
+$interval->invert = 1;
+$date4->add($interval);
+echo $interval->format('(%y -years, %m -mounts, %d -days, %h -hours, %i -minutes) ').'from today is ' . $date4->format('d-m-Y G:i:s') . "<br/><br/>" . PHP_EOL ;
+
+?>
---
+++ Php_Date_Time_Example/02_DayOfWeek.php
@@ -0,0 +1,24 @@
+<?php
+echo 'Today is '.date("d")." ".date("m")." ".date("Y")." ".date("h:i:s A").' - ';
+echo jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 2 )."<br/><br/>";
+echo 'Today is '.jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 0 ).'th day of week - ';
+echo jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 1 )."<br/><br/>";
+
+$today = new DateTime();
+$interval = new DateInterval('P2Y');
+$interval->invert = 1;
+$day_before_1Y = new DateTime();
+$day_before_1Y->add($interval);
+echo 'The day of week before 2 year has been - ';
+    switch( $day_before_1Y->format('w')){
+        case 0: echo $day_before_1Y->format('jS F, Y').' - Sunday';break;
+        case 1: echo $day_before_1Y->format('jS F, Y').' - Monday';break;
+        case 2: echo $day_before_1Y->format('jS F, Y').' - Tuesday';break;
+        case 3: echo $day_before_1Y->format('jS F, Y').' - Wednesday';break;
+        case 4: echo $day_before_1Y->format('jS F, Y').' - Thursday';break;
+        case 5: echo $day_before_1Y->format('jS F, Y').' - Friday';break;
+        case 6: echo $day_before_1Y->format('jS F, Y').' - Saturday';break;
+        default: echo 'Error';break;
+    }
+
+?>
