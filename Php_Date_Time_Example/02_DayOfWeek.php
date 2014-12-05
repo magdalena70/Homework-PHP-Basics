f5f1406cdf8348b5e6435f79d25aff3a4fb19c65
Magdalena Petrova <magik70@abv.bg>
2014-12-05 22:05:45 +0200

    02_DayOfWeek

--- Php_Date_Time_Example/02_DayOfWeek.php
+++ Php_Date_Time_Example/02_DayOfWeek.php
@@ -8,17 +8,17 @@ echo jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 1 
 $today = new DateTime();
 $interval = new DateInterval('P2Y');
 $interval->invert = 1;
-$day_before_1Y = new DateTime();
-$day_before_1Y->add($interval);
+$day_before_2Y = new DateTime();
+$day_before_2Y->add($interval);
 echo 'The day of week before 2 year has been - ';
     switch( $day_before_1Y->format('w')){
-        case 0: echo $day_before_1Y->format('jS F, Y').' - Sunday';break;
-        case 1: echo $day_before_1Y->format('jS F, Y').' - Monday';break;
-        case 2: echo $day_before_1Y->format('jS F, Y').' - Tuesday';break;
-        case 3: echo $day_before_1Y->format('jS F, Y').' - Wednesday';break;
-        case 4: echo $day_before_1Y->format('jS F, Y').' - Thursday';break;
-        case 5: echo $day_before_1Y->format('jS F, Y').' - Friday';break;
-        case 6: echo $day_before_1Y->format('jS F, Y').' - Saturday';break;
+        case 0: echo $day_before_2Y->format('jS F, Y').' - Sunday';break;
+        case 1: echo $day_before_2Y->format('jS F, Y').' - Monday';break;
+        case 2: echo $day_before_2Y->format('jS F, Y').' - Tuesday';break;
+        case 3: echo $day_before_2Y->format('jS F, Y').' - Wednesday';break;
+        case 4: echo $day_before_2Y->format('jS F, Y').' - Thursday';break;
+        case 5: echo $day_before_2Y->format('jS F, Y').' - Friday';break;
+        case 6: echo $day_before_2Y->format('jS F, Y').' - Saturday';break;
         default: echo 'Error';break;
     }
 
