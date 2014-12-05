418724b3d08df6c242a1455c5ee3534b955d78e2
Magdalena Petrova <magik70@abv.bg>
2014-12-05 17:49:07 +0200

    Php_Date_Time_Example

--- Php_Date_Time_Example/02_DayOfWeek.php
+++ Php_Date_Time_Example/02_DayOfWeek.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Europe/Sofia');
 echo 'Today is '.date("d")." ".date("m")." ".date("Y")." ".date("h:i:s A").' - ';
 echo jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 2 )."<br/><br/>";
 echo 'Today is '.jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 0 ).'th day of week - ';
